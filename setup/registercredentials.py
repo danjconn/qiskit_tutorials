b35fe53f0e881a0508f215494cab75fa462c2e3f
Dan Conn <dan@danconn.solutions>
2019-05-13 10:36:51 +0100

    Refactor structure

--- setup/registercredentials.py
+++ setup/registercredentials.py
@@ -1,4 +1,4 @@
-import config
+from setup import config
 from qiskit import IBMQ
 print("Registering credentials stored in config.py. Overwriting any existing ones.")
 IBMQ.save_account(config.IBM_Q_API_TOKEN, 'https://quantumexperience.ng.bluemix.net/api', True)
